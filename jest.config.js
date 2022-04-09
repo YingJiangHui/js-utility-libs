90bda01fb6b38dc1536691eded1c70eec6362269
Jh <473380917@qq.com>
2022-04-09 19:17:52 +0800

    build: tiny up configs

--- jest.config.js
+++ jest.config.js
@@ -3,12 +3,6 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   extensionsToTreatAsEsm: ['.ts'],
-  transform: {
-    // "\\.[jt]s?$": "babel-jest",
-    // '^.+\\.ts?$': 'ts-jest',
-  },
-  // testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
-  // moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   globals: {
     'ts-jest': {
       useESM: true,
---
+++ packages/image-grayscale-percentage/jest.config.js
@@ -0,0 +1,6 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+// @ts-ignore
+const jestConfig = require('../../jest.config.js')
+module.exports = {
+  ...jestConfig,
+};
